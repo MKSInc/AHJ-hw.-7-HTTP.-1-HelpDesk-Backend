2eca5aefcb3a74077cb5cdb399c788114c9a71cc
Maksim <pyshkov.maksim@gmail.com>
2020-12-30 17:22:43 +0200

    feat: task completed

--- server.js
+++ server.js
@@ -1,13 +1,49 @@
-const Koa = require('koa');
-const app = new Koa();
+const { TicketsDB } = require('./src/TicketsDB');
+const ticketsDB = new TicketsDB();
 
-const PORT = process.env.PORT || 3000;
-
-const startMsg = `Koa server has been started on port ${PORT} ...`;
-
-app.use(async (ctx, next) => {
-   console.log('Test');
-   ctx.body = startMsg;
+ticketsDB.createTicket({
+   name: 'Поменять краску в принтере',
+   status: 'todo',
+   description: 'Описание для замены краски в принтере.'
 });
 
-app.listen(PORT, () => console.log(startMsg));
+ticketsDB.createTicket({
+   name: 'Переустановить Windows',
+   status: 'todo',
+   description: 'Описание для переустановки Windows.'
+});
+
+const Koa = require('koa');
+const koaBody = require('koa-body');
+const app = new Koa();
+const PORT = process.env.PORT || 3000;
+
+// Чтобы Koa мог обработать запрос, в теле которого передано FormData
+app.use(koaBody({
+   multipart: true,
+}));
+
+app.use(async (ctx, next) => {
+   // ctx.response.set('Access-Control-Allow-Origin', 'http://localhost:8080');
+   ctx.response.set('Access-Control-Allow-Origin', 'https://mksinc.github.io/AHJ-hw.-7-HTTP.-1-HelpDesk-Frontend/');
+
+   let method;
+   if (ctx.request.method === 'GET') ({ method } = ctx.request.query);
+   else if (ctx.request.method === 'POST') ({ method } = ctx.request.body);
+
+   let response;
+
+   switch (method) {
+      case 'allTickets': response = ticketsDB.getTickets();
+         break;
+      case 'ticketById': response = ticketsDB.getTicketFull(ctx.request.query.id);
+         break;
+      case 'createTicket': response = 'Ticket created'; ticketsDB.createTicket(ctx.request.body);
+         break;
+      default: response = `Unknown method '${method}' in request parameters`; ctx.status = 404;
+   }
+
+   ctx.body = response;
+});
+
+app.listen(PORT, () => console.log(`Koa server has been started on port ${PORT} ...`));
---
+++ src/Ticket.js
@@ -0,0 +1,20 @@
+class Ticket {
+   constructor({ id, name, status, created }) {
+      this.id = id;
+      this.name = name;
+      this.status = status;
+      this.created = created;
+   }
+}
+
+class TicketFull extends Ticket {
+   constructor({ id, name, status, created, description }) {
+      super({ id, name, status, created });
+      this.description = description;
+   }
+}
+
+module.exports = {
+   Ticket,
+   TicketFull,
+};
